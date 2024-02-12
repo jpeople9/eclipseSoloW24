24340aff7438eae53959fc8e5f8e40812c546f5c
jpeoples <jpeople9@emich.edu>
2024-02-12 13:47:37 -0800

    Fixed merge error

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
-		System.out.println("alternate Hello github Project-- Changed This Message -- decided to change this");
+		System.out.println("alternate Hello github Project-- Change This Message -- decided to change this");
 
 
 	}
